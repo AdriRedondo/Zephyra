76455c08b44efc83a44c3a511ffe736ebb63a5c6
Alexcanora <acanora02@gmail.com>
2025-10-12 10:33:09 +0200

    Pequeños cambio

--- js/formulario.js
+++ js/formulario.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const form = document.getElementById("reservas-form");
+    const form = document.getElementById("reservas-form-id");
     form.addEventListener("submit", (event) => {
         const nombre = document.getElementById("nombre").value;
         const correo = document.getElementById("correo").value;
