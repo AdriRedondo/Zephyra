3fe99bd130df3bfc99b8c52b76070ce16884229f
Alexcanora <acanora02@gmail.com>
2025-10-12 11:13:30 +0200

    Colores de los bordes de los inpits hechos

--- js/formulario.js
+++ js/formulario.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     const form = document.getElementById("reservas-form-id");
     form.addEventListener("submit", (event) => {
+        event.preventDefault();
         const nombre = document.getElementById("nombre").value;
         const correo = document.getElementById("correo").value;
         const vehiculo = document.getElementById("vehiculos-form").value;
@@ -20,53 +21,72 @@ document.addEventListener("DOMContentLoaded", () => {
         var es_correcto = true;
 
         // Validación nombre y apellidos
-        if (!/^[a-zA-Z]{3,}$/.test(nombre)) {
+        if (nombre == "" || !/^[a-zA-Z\s]{3,}$/.test(nombre)) {
             es_correcto = false;
-            document.getElementById("nombre").classList.toggle("wrong-input");
+            document.getElementById("nombre").classList.add("wrong-input");
+            document.getElementById("nombre").classList.remove("right-input");
+
         }
         else {
-            document.getElementById("nombre").classList.toggle("right-input");
+            document.getElementById("nombre").classList.add("right-input");
+            document.getElementById("nombre").classList.remove("wrong-input");
+
         }
 
         // Validación correo
         //else if (!/^(a-zA-Z0-9)+@+(a-zA-Z0-9)\.+{2,}/.test(correo)) {
-        if (!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/) {
+        if (correo == "" || !/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(correo)) {
             es_correcto = false;
+            document.getElementById("correo").classList.add("wrong-input");
+            document.getElementById("correo").classList.remove("right-input");
+
         }
         else {
+            document.getElementById("correo").classList.add("right-input");
+            document.getElementById("correo").classList.remove("wrong-input");
 
         }
 
         // Validación fecha ini
-        if (fecha_ini < hoy) {
+        if (inicio == "" || fecha_ini < hoy) {
             es_correcto = false;
+            document.getElementById("inicio").classList.add("wrong-input");
+            document.getElementById("inicio").classList.remove("right-input");
+
         }
         else {
+            document.getElementById("inicio").classList.add("right-input");
+            document.getElementById("inicio").classList.remove("wrong-input");
 
         }
 
         // Validación fecha fin
-        if (fecha_fin < hoy) {
-
-        }
-        else {
-
-        }
-
-        // Validación fecha ini y fin
-        if (fecha_ini >= fecha_ini) {
+        if (fin == "" || fecha_fin < hoy || fecha_ini >= fecha_fin) {
             es_correcto = false;
+            document.getElementById("fin").classList.add("wrong-input");
+            document.getElementById("fin").classList.remove("right-input");
         }
         else {
-
+            document.getElementById("fin").classList.add("right-input");
+            document.getElementById("fin").classList.remove("wrong-input");
         }
 
-        if (!es_correcto) {
-            //event.preventDefault();
+        if (telefono == "" || !/^[0-9]{9}$/.test(telefono)) {
+            es_correcto = false;
+            document.getElementById("telefono").classList.add("wrong-input");
+            document.getElementById("telefono").classList.remove("right-input");
+        }
+        else {
+            document.getElementById("telefono").classList.add("right-input");
+            document.getElementById("telefono").classList.remove("wrong-input");
         }
 
-
+        if (es_correcto) {
+            form.submit()
+            alert("La reserva ha sido realizada con exito.")
+        }
+        else {
+            alert("Campos erróneos.")
+        }
     });
-
-
 });
