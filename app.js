942f2a81c0673be40428aea2b9f023c95d01d5c4
Alexcanora <acanora02@gmail.com>
2025-11-04 18:33:03 +0100

    Empiezo a meter las plantillas

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ const PORT = 3000;
 // Middleware para servir toda la carpeta public como estática
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.set('view engine', 'ejs');
+
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
 app.use(express.urlencoded({ extended: false }));
 
 app.post('/submit_contact', (req, res) => {
