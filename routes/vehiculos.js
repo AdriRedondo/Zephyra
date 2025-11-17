bfee55cfd1b2d12f12fb677f5ded53197c93883e
AdriRedondo <adriredogom@gmail.com>
2025-11-17 16:59:56 +0100

    Cambio gordete en la pestaña de vehiculos

--- routes/vehiculos.js
+++ routes/vehiculos.js
@@ -1,36 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
+const fs = require('fs');
 
-const vehiculosDisponibles = [
-    // Coches
-    { matricula: '1234-BCD', marca: 'Seat', modelo: 'León e-Hybrid', autonomia: 60, tipo: 'coche' },
-    { matricula: '5678-FGH', marca: 'Renault', modelo: 'Zoe E-Tech', autonomia: 395, tipo: 'coche' },
-    { matricula: '9012-JKL', marca: 'Peugeot', modelo: 'e-208', autonomia: 362, tipo: 'coche' },
+const dataPath = path.join(__dirname, '../data.json');
+const data = JSON.parse(fs.readFileSync(dataPath, 'utf8'));
 
-    // Motos
-    { matricula: '3456-MNP', marca: 'Silence', modelo: 'S01', autonomia: 133, tipo: 'moto' },
-    { matricula: '7890-RST', marca: 'Vectrix', modelo: 'VX-1 Li+', autonomia: 200, tipo: 'moto' },
+const vehiculosDisponibles = data.vehiculos;
 
-    // Furgonetas
-    { matricula: '1122-VXZ', marca: 'Citroen', modelo: 'e-Berlingo', autonomia: 280, tipo: 'furgoneta' },
-    { matricula: '3344-YZA', marca: 'Ford', modelo: 'E-Transit', autonomia: 317, tipo: 'furgoneta' },
-];
 
 router.get('/es-vehiculos', (req, res) => {
 
     const tipoFiltro = req.query.tipo;
+    const marcaFiltro = req.query.marca;
 
     let vehiculosFiltrados = vehiculosDisponibles;
 
     if (tipoFiltro) {
-        const filtroNormalizado = tipoFiltro.toLowerCase();
-
-        vehiculosFiltrados = vehiculosDisponibles.filter(vehiculo =>
-            vehiculo.tipo.toLowerCase() === filtroNormalizado
+        vehiculosFiltrados = vehiculosFiltrados.filter(
+            v => v.tipo.toLowerCase() === tipoFiltro.toLowerCase()
         );
-        console.log(`Filtro aplicado: tipo = ${tipoFiltro}. Vehículos encontrados: ${vehiculosFiltrados.length}`);
-    } else {
-        console.log("No se aplicó filtro, mostrando todos los vehículos.");
+    }
+
+    if (marcaFiltro) {
+        vehiculosFiltrados = vehiculosFiltrados.filter(
+            v => v.marca.toLowerCase() === marcaFiltro.toLowerCase()
+        );
     }
 
 
