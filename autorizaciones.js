d923dd04bcd852d2de4ff1ec142efcdb350a0f6c
Alexcanora <acanora02@gmail.com>
2025-11-25 12:17:07 +0100

    Update autorizaciones.js

--- autorizaciones.js
+++ autorizaciones.js
@@ -1,24 +1,33 @@
+//Función que revisa si el usuario tiene autorización de administrador
 function requiredAdminId(req, res, next) {
     if (req.session.usuario && req.session.usuario.rol === 'admin') {
         console.log('Si estás autorizado');
         next();
     }
     else {
+        //Si no está autorizado, lanza error 401
         console.log('No estás autorizado');
-        return res.redirect('/es-inicio');
+        const error = 'Error 401';
+        const texto1 = 'Acceso no autorizado.';
+        const texto2 = 'Se ha intentado acceder a una página a la que no se tiene autorización.'
+        return res.status(401).render("errors", { error, texto1, texto2 });
     }
 }
 
+//Función que revisa si el usuario tiene autorización de administrador o empleado
 function requiredEmployeeId(req, res, next) {
     if (req.session.usuario && req.session.usuario.rol === 'empleado') {
         console.log('Si estás autorizado');
         next();
     }
     else {
+        //Si no está autorizado, lanza error 401
         console.log('No estás autorizado');
-        return res.redirect('/es-inicio');
+        const error = 'Error 401';
+        const texto1 = 'Acceso no autorizado.';
+        const texto2 = 'Se ha intentado acceder a una página a la que no se tiene autorización.'
+        return res.status(401).render("errors", { error, texto1, texto2 });
     }
-
 }
 
 module.exports = {
